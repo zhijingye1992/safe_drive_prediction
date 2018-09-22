a9078a05a6b3273db821ac4465d37bb304e93df7
Zhijing Ye <ZhijingYe@Zhijings-MacBook-Pro.local>
2018-09-21 23:21:27 -0500

    analysis 2

---
+++ data_analysis.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv('/Users/ZhijingYe/Documents/ipython_notebook/HW/practice/Safe_Driver_Prediction/data/train.csv',
+                 index_col=False)
+print(df.head())
