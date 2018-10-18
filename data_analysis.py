bd69dca8c907daeb0969d7e4f72b5c626719b8f1
Zhijing Ye <ZhijingYe@Zhijings-MacBook-Pro.local>
2018-10-17 23:00:02 -0500

    change

--- data_analysis.py
+++ data_analysis.py
@@ -2,3 +2,5 @@ import pandas as pd
 df = pd.read_csv('/Users/ZhijingYe/Documents/ipython_notebook/HW/practice/Safe_Driver_Prediction/data/train.csv',
                  index_col=False)
 print(df.head())
+
+from sklearn import linear_model
